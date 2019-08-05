c80eb005ad9264727b29ec1aad4a1fc82a8b1e6e
strike970124 <41593886+strike970124@users.noreply.github.com>
2019-08-04 19:46:20 -0500

    v2

--- typicalFunctions/runHere.php
+++ typicalFunctions/runHere.php
@@ -7,4 +7,24 @@
  * Good luck!
  */
 
- // Write your code here.
+$input = array(5,43,56,37);
+$tipical = new TypicalFunctions();
+$result = $tipical->getSuperArray($input);
+print_r($result);
+
+$tipical = new TypicalFunctions();
+$input2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+$result = $tipical->capitalizeFirstLetters($input2);
+print $result;
+
+$input = array("Locomotive","Ant","Toucan","Dolphin","Stadium","Tree");
+$result = $tipical->getShortestLongestValue($input);
+print($result);
+
+
+$input = array("Italia" =>"Roma", "Belgium" => "Brussels", "Ireland" => "Dublin" ,"Greece" => "Athens");
+$result = $tipical->getCapitalCities($input);
+
+$input = 25;
+$result = $tipical->getSumPrimeNumbers($input);
+print($result);
--- typicalFunctions/typicalFunctions.php
+++ typicalFunctions/typicalFunctions.php
@@ -137,30 +137,6 @@ class TypicalFunctions {
         $number += 1;
     }
     return $sum;
-  }
-  
+  } 
 }
-/*
-$input = array(5,43,56,37);
-$tipical = new TypicalFunctions();
-$result = $tipical->getSuperArray($input);
-print_r($result);*/
-
-/*$tipical = new TypicalFunctions();*/
-/*$input2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-$result = $tipical->capitalizeFirstLetters($input2);
-print $result;*/
-/*
-$input = array("Locomotive","Ant","Toucan","Dolphin","Stadium","Tree");
-$result = $tipical->getShortestLongestValue($input);
-print($result);
-*/
-
-/*$input = array("Italia" =>"Roma", "Belgium" => "Brussels", "Ireland" => "Dublin" ,"Greece" => "Athens");
-$result = $tipical->getCapitalCities($input);*/
-
-/*$input = 25;
-$result = $tipical->getSumPrimeNumbers($input);
-print($result);*/
-
 ?>
